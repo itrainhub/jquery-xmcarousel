26e90aa7b82115571481b8c2e7d947396ef4b3b3
xiaoming <wumingfu@1000phone.com>
2018-10-19 11:02:53 +0800

    轮播图基本功能及测试

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,41 @@
 const gulp = require("gulp"),
   uglify = require("gulp-uglify"),
-  rename = require("gulp-rename");
+  rename = require("gulp-rename"),
+  sass = require("gulp-sass");
 
-gulp.task("js", ()=>{
+gulp.task("js-uncompressed", ()=>{
   return gulp.src("./src/*.js")
     .pipe(uglify())
     .pipe(rename({
-      suffix: "-min"
+      suffix: ".min"
     }))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("copy", ()=>{
-  return gulp.src(["./src/*.*", "!src/*.js"])
+gulp.task("js-copy", ()=>{
+  return gulp.src(["./src/*.js"])
     .pipe(gulp.dest("./dist"));
 });
+gulp.task("js", ["js-uncompressed", "js-copy"]);
 
-gulp.task("default", ["copy", "js"]);
+gulp.task("sass-uncompressed", ()=>{
+  return gulp.src("./src/scss/*.scss")
+    .pipe(sass({outputStyle: "expanded"}))
+    .pipe(gulp.dest("./dist"));
+});
+gulp.task("sass-min", ()=>{
+  return gulp.src("./src/scss/*.scss")
+    .pipe(sass({outputStyle: "compressed"}))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("./dist"));
+});
+gulp.task("sass", ["sass-uncompressed", "sass-min"]);
+
+gulp.task("watch", ()=>{
+  gulp.watch("./src/*.js", ["js"]);
+  gulp.watch("./src/scss/*.scss", ["sass"]);
+});
+
+gulp.task("default", ["js", "sass", "watch"]);
